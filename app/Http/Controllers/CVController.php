47658c8387274ebc67b668745fae241e79b121aa
Hamza Maach <hamzamaach56@gmail.com>
2023-06-18 22:54:30 +0100

    generate the cv

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\CV;
+use App\Models\User;
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 
 class CVController extends Controller
@@ -14,6 +16,49 @@ class CVController extends Controller
     {
         //
     }
+
+    public function generate(string $id)
+    {
+        $stagiaire = User::with('cv', 'interets', 'groupe', 'competences', 'experiences', 'formations', 'groupe.filiere')
+            ->where('id', $id)
+            ->where('role', 'stagiaire')
+            ->first();
+
+        if ($stagiaire) {
+            if ($stagiaire->cv) {
+                $birthDate = $stagiaire->cv->dateNais;
+                $now = \Carbon\Carbon::now();
+                $age = $now->diffInYears($birthDate);
+                $stagiaire->age = $age;
+                $nom = $stagiaire->nom;
+                $email = $stagiaire->email;
+                $tel = $stagiaire->tel;
+                $cv = $stagiaire->cv;
+                $formations = $stagiaire->formations;
+                $experiences = $stagiaire->experiences;
+                $competences = $stagiaire->competences;
+                $interets= $stagiaire->interets;
+                $prenom = $stagiaire->prenom;
+                $fullName = $nom." ".$prenom;
+                $stagiaire->fullName = $fullName;
+                $data = $stagiaire->toArray();
+                if (view()->exists('documents.cv')) {
+                    $pdf = \Barryvdh\DomPDF\Facade\Pdf::loadView('documents.cv', compact('fullName', 'email', 'tel', 'age', 'cv', 'formations', 'experiences', 'competences','interets'));
+                    return $pdf->download($nom . "_" . $prenom.'.pdf');
+                } else {
+                    return response()->json([
+                        "message" => "View 'documents.cv' not found"
+                    ], 404);
+                }
+            }
+        }
+
+        return response()->json([
+            "message" => "404"
+        ], 404);
+    }
+
+
 
     /**
      * Show the form for creating a new resource.
